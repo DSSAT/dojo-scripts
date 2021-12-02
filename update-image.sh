b923a93c02294f912a24a737a7932d36cb54ef3a
Christopher Villalobos <cvillalobos@ufl.edu>
2021-12-02 07:19:23 -0500

    Use repository location for scripts

--- update-image.sh
+++ update-image.sh
@@ -30,10 +30,10 @@ curl --create-dirs -o $HOME/downloads/global-base-latest.tar.bz2 https://data.ag
 cd /data && tar xjvf $HOME/downloads/global-base-latest.tar.bz2
 
 # Download the scripts from upstream
-sudo curl --create-dirs -o /usr/local/bin/load_kenya https://data.agmip.org/darpa/load_kenya.sh
-sudo curl --create-dirs -o /usr/local/bin/load_ethiopia https://data.agmip.org/darpa/load_ethiopia.sh
-sudo curl --create-dirs -o /usr/local/bin/pipeline https://data.agmip.org/darpa/pipeline.sh
-sudo curl --create-dirs -o /usr/local/bin/baseline https://data.agmip.org/darpa/baseline.sh
+sudo curl --create-dirs -o /usr/local/bin/load_kenya https://data.agmip.org/darpa/dojo/load_kenya.sh
+sudo curl --create-dirs -o /usr/local/bin/load_ethiopia https://data.agmip.org/darpa/dojo/load_ethiopia.sh
+sudo curl --create-dirs -o /usr/local/bin/pipeline https://data.agmip.org/darpa/dojo/pipeline.sh
+sudo curl --create-dirs -o /usr/local/bin/baseline https://data.agmip.org/darpa/dojo/baseline.sh
 sudo chmod 755 /usr/local/bin/load_kenya && sudo chown clouseau /usr/local/bin/load_kenya
 sudo chmod 755 /usr/local/bin/load_ethiopia && sudo chown clouseau /usr/local/bin/load_ethiopia
 sudo chmod 755 /usr/local/bin/pipeline  && sudo chown clouseau /usr/local/bin/pipeline
