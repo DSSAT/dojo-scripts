01e0c0f126e1bb09709730f42744e840b06e5252
Christopher Villalobos <cvillalobos@ufl.edu>
2022-02-16 10:01:40 -0500

    Improve variable readability
    * Rename some of the variables to make them more understandable.
    
    * Fix SEASON-v problem

--- pipeline.sh
+++ pipeline.sh
@@ -13,9 +13,9 @@ LOW_PRODUCTION_PER_PERSON=200
 # Stop modifying here
 WEATHER_PATH="/data/ethiopia/weather"
 PYTHIA_PP="pp.csv"
-RAW_PYTHIA_DIR="/tmp/results"
-RAW_PYTHIA_OUT="/userdata/out"
-PYTHIA_OUT_FILE="${RAW_PYTHIA_DIR}/${PYTHIA_PP}"
+ORIG_PYTHIA_DIR="/tmp/results"
+NEW_PYTHIA_DIR="/userdata/out"
+WORK_FILE="${NEW_PYTHIA_DIR}/${PYTHIA_PP}"
 BASELINE_ANALYSIS_DIR="/data/baseline"
 ANALYSIS_DIR="/userdata/out"
 CURRENT=`pwd`
@@ -28,12 +28,12 @@ tar xjvf $HOME/downloads/ethiopia-weather-latest.tar.bz2
 rm $HOME/downloads/ethiopia-weather-latest.tar.bz2
 
 # Actually run the pipeline
-rm -rf $RAW_PYTHIA_OUT && \
-mkdir -p $RAW_PYTHIA_OUT && \
+rm -rf $NEW_PYTHIA_DIR && \
+mkdir -p $NEW_PYTHIA_DIR && \
 pythia --clean-work-dir --all /userdata/pythia.json && \
-cp "${RAW_PYTHIA_DIR}/${PYTHIA_PP}" $PYTHIA_OUT_FILE
+cp "${ORIG_PYTHIA_DIR}/${PYTHIA_PP}" $WORK_FILE
 cd /opt/pythia-analytics && \
-/usr/bin/Rscript aggregate-pythia-outputs.R -f LATITUDE LONGITUDE MGMT HYEAR CR SEASON-v PRODUCTION CROP_FAILURE_AREA -t HARVEST_AREA -o NICM -a HWAM -c $CROP_FAILURE_THRESHOLD -l $LOW_PRODUCTION_PER_PERSON_THRESHOLD -- $PYTHIA_OUT_FILE $ANALYSIS_DIR/stage_2.csv && \
-rm -rf $RAW_PYTHIA_DIR && \
+/usr/bin/Rscript aggregate-pythia-outputs.R -f LATITUDE LONGITUDE MGMT HYEAR CR SEASON -v PRODUCTION CROP_FAILURE_AREA -t HARVEST_AREA -o NICM -a HWAM -c $CROP_FAILURE_THRESHOLD -l $LOW_PRODUCTION_PER_PERSON_THRESHOLD -- $WORK_FILE $ANALYSIS_DIR/stage_2.csv && \
+rm -rf $ORIG_PYTHIA_DIR && \
 rm -rf $WEATHER_PATH && \
 cd $CURRENT
