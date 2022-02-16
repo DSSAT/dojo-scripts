4f33693bdc4b86aad8b96d02e2dd2d37a773d1b7
Christopher Villalobos <cvillalobos@ufl.edu>
2022-02-16 08:12:08 -0500

    Fix pythia path
    * Pythia is installed by the image, available in $PATH.

--- pipeline.sh
+++ pipeline.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+# This is a minimized version of the entire pipeline. We are currently working on how to get the visualization for the different
+# levels of output to work as we want in the visualization system. Until that time, we are only providing the minimally processed
+# data.
+
 # Modify these variables as needed
 ADMLV_LEVEL="ADMLV0"
 ADMLV_MATCH="Ethiopia"
 CROP_FAILURE_THRESHOLD=200
 LOW_PRODUCTION_PER_PERSON=200
+
 # Stop modifying here
 WEATHER_PATH="/data/ethiopia/weather"
 PYTHIA_PP="pp.csv"
@@ -25,7 +30,7 @@ rm $HOME/downloads/ethiopia-weather-latest.tar.bz2
 # Actually run the pipeline
 rm -rf $RAW_PYTHIA_OUT && \
 mkdir -p $RAW_PYTHIA_OUT && \
-/home/clouseau/.local/bin/pythia --clean-work-dir --all /userdata/pythia.json && \
+pythia --clean-work-dir --all /userdata/pythia.json && \
 cp "${RAW_PYTHIA_DIR}/${PYTHIA_PP}" $PYTHIA_OUT_FILE
 cd /opt/pythia-analytics && \
 /usr/bin/Rscript aggregate-pythia-outputs.R -f LATITUDE LONGITUDE MGMT HYEAR CR SEASON-v PRODUCTION CROP_FAILURE_AREA -t HARVEST_AREA -o NICM -a HWAM -c $CROP_FAILURE_THRESHOLD -l $LOW_PRODUCTION_PER_PERSON_THRESHOLD -- $PYTHIA_OUT_FILE $ANALYSIS_DIR/stage_2.csv && \
