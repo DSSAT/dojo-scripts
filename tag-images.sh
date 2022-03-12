0773648c0803cc7969dd5e88bd877694569571db
Christopher Villalobos <cvillalobos@ufl.edu>
2022-03-12 00:13:08 -0500

    Add tag-images.sh
    * Dynamically tag the large directory of images.
    * Includes captions.

---
+++ tag-images.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+IMAGE_PATH="/home/frostbytten/Downloads/test/"
+
+# === DO NOT MODIFY BELOW THIS LINE ===
+CURRENT=$(pwd)
+PREAMBLE="This image shows the model's sensitivity response to "
+
+cd ${IMAGE_PATH}
+for f in *; do
+    echo $f
+    v=$(echo $f | awk -F'-' '{print $1}' | sed 's/_/ /g')
+    case $v in
+    "planting date")
+        desc="shifting the planting date"
+        ;;
+    "fertilizer")
+        desc="increased fertilizer[N] amounts"
+        ;;
+    *)
+        echo "error: unsupport variable ($v)" 1>&2
+        exit 1
+        ;;
+    esac
+    e=$(echo $f | grep -c 'Ethiopia')
+    r=$(echo $f | awk -F'-' '{print $2}' | sed 's/\.png//g' | sed 's/_/ /g' | sed 's/Nations/Nations,/g')
+    if [[ $e -eq 1 ]]; then
+        desc="${desc} for all of Ethiopia's growing regions."
+    else
+        desc="${desc} for the ${r} region."
+    fi
+    dojo tag "${f}" "${PREAMBLE}${desc}"
+done
+
+cd ${CURRENT}
