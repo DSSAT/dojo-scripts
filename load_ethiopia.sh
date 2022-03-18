21dacc8a596872c5de4662da89447ac8c35e5359
Christopher Villalobos <cvillalobos@ufl.edu>
2022-03-18 15:54:06 -0400

    Use dojo-scripts for the base scripts
    * No longer downloading copies made of dojo-scripts.
    * Use dojo-scripts for pipeline and load_ethiopia.
    * Use dojo-scripts for the runs directly (symlink)

--- load_ethiopia.sh
+++ load_ethiopia.sh
@@ -4,14 +4,11 @@ CURRENT=`pwd`
 
 # Fetch Kenya data
 echo "Downloading data for Ethiopia..."
-curl --create-dirs -o $HOME/downloads/ethiopia-runs-latest.tar.bz2 https://data.agmip.org/darpa/ethiopia-runs-latest.tar.bz2
 curl --create-dirs -o $HOME/downloads/ethiopia-base-latest.tar.bz2 https://data.agmip.org/darpa/ethiopia-base-latest.tar.bz2
 
 echo "Extracting data..."
 cd /data
 tar xjvf $HOME/downloads/ethiopia-base-latest.tar.bz2
-cd /userdata
-tar xjvf $HOME/downloads/ethiopia-runs-latest.tar.bz2
 echo "DONE!"
 
 cd $CURRENT
--- update-image.sh
+++ update-image.sh
@@ -6,17 +6,15 @@ sudo chown -R clouseau /opt && sudo chown -R clouseau /data && sudo chown -R clo
 
 # Download the base data to the image
 curl --create-dirs -o $HOME/downloads/global-base-latest.tar.bz2 https://data.agmip.org/darpa/global-base-latest.tar.bz2 && \
-cd /data && tar xjvf $HOME/downloads/global-base-latest.tar.bz2
+    cd /data && tar xjvf $HOME/downloads/global-base-latest.tar.bz2
 
-# Download the scripts from upstream
-sudo curl --create-dirs -o /usr/local/bin/load_kenya https://data.agmip.org/darpa/dojo/load_kenya.sh
-sudo curl --create-dirs -o /usr/local/bin/load_ethiopia https://data.agmip.org/darpa/dojo/load_ethiopia.sh
-sudo curl --create-dirs -o /usr/local/bin/pipeline https://data.agmip.org/darpa/dojo/pipeline.sh
-sudo curl --create-dirs -o /usr/local/bin/baseline https://data.agmip.org/darpa/dojo/baseline.sh
-sudo chmod 755 /usr/local/bin/load_kenya && sudo chown clouseau /usr/local/bin/load_kenya
-sudo chmod 755 /usr/local/bin/load_ethiopia && sudo chown clouseau /usr/local/bin/load_ethiopia
-sudo chmod 755 /usr/local/bin/pipeline  && sudo chown clouseau /usr/local/bin/pipeline
-sudo chmod 755 /usr/local/bin/baseline && sudo chown clouseau /usr/local/bin/baseline
+# Download the scripts from git
+cd /usr/local/src && \
+    sudo git clone https://github.com/DSSAT/dojo-scripts && \
+    chown -R clouseau dojo-scripts
+
+sudo ln -s /usr/local/src/dojo-scripts/pipeline.sh /usr/local/bin/pipeline
+sudo ln -s /usr/local/src/dojo-scripts/load_ethiopia.sh /usr/local/bin/load_ethiopia
 
 # Handle the user install of R packages
 cd /opt/pythia-analytics && \
