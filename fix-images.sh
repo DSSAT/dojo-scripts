efe367416dd870edc02b8fc0787088dab30437da
Christopher Villalobos <cvillalobos@ufl.edu>
2022-04-01 09:25:09 -0400

    Fix regions being skipped
    * Filter out the `Ethiopia-` for regions.

--- fix-images.sh
+++ fix-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for f in *; do
-   v=$(echo $f | sed -e 's/[_]\+/-/g' | sed -e 's/ing-da/ing_da/g' | sed -e 's/[,\ ]\+/_/g')
+   v=$(echo $f | sed -e 's/[_]\+/-/g' | sed -e 's/ing-da/ing_da/g' | sed -e 's/[,\ ]\+/_/g' | sed -e 's/Ethiopia-//g')
    mv "$f" $v
 done
